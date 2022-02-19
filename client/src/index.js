f4fc3a2a7390b94ab677bbe249807286fa218f67
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-02-19 13:46:33 -0700

    fixed file structures

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
 
-var path = __dirname + '/views/'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-var router = express.Router()
-
-app.use('/', function (req, res) {
-    res.sendFile(path + 'index.html')
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
